a33a3bc1d5f08bcb5ea40f27fb7258ced7ced055
Pahan <5972140@gmail.com>
2021-10-29 20:10:18 +0300

    Bug fix: quantity of pict outputs properly

--- services.py
+++ services.py
@@ -8,11 +8,11 @@ likes = None
 
 
 def connect_pixabay(per_page: int, category: str):
-    # secret_key = os.environ.get("pixabay_key")
-    secret_key = "23797809-8f26efa984bdf9135a7c4b2b3"
-    url = f"https://pixabay.com/api/?key={secret_key}&category={category}&p={per_page}&image_type=photo"
+    secret_key = os.environ.get("pixabay_key")
+    url = f"https://pixabay.com/api/?key={secret_key}&category={category}&per_page={per_page}&image_type=photo"
     return requests.get(url).json()
 
 
 if __name__ == "__main__":
+    # Check
     print(connect_pixabay(5, "flowers"))
